947bc1cae02e587c0792876b04243cbd06e0a833
TC Teo <tsechin@tsechin.com>
2025-06-05 23:41:40 +1000

    clean up a bit

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,11 @@
 #include <SPI.h>
 #include <Wire.h>
 
-Adafruit_MCP9808 tempSensor;
-
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library.
-// On an arduino UNO:       A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
-#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS                                                         \
-  0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3D // 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#define NUMFLAKES 10 // Number of snowflakes in the animation example
 
 #define STRINGIZE(arg) #arg
 #define SVAL(x) STRINGIZE(x)
@@ -39,9 +29,12 @@ const char *wifi_ssid = WIFI_SSID_SVAL;
 const char *wifi_pass = WIFI_PASS_SVAL;
 uint8_t macAddr[WL_MAC_ADDR_LENGTH];
 char macStr[WL_MAC_ADDR_LENGTH * 2 + 1];
-const int ledPin = D4;
+
+Adafruit_MCP9808 tempSensor;
 WiFiUDP wifiUdp;
 NTP ntp(wifiUdp);
+
+const int ledPin = D4; // GPIO2=D4 for LED on ESP8266 NodeMCU/D1mini
 
 void setup_wifi() {
   delay(10);
@@ -67,7 +60,7 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 
   display.clearDisplay();
-  display.setCursor(0, 0); // Start at top-left corner
+  display.setCursor(0, 0);
   display.setTextSize(1);
   display.println(F("wifi connected."));
   display.println(WiFi.localIP());
@@ -77,7 +70,8 @@ void setup_wifi() {
 }
 
 void setup() {
-  pinMode(ledPin, OUTPUT); // GPIO2=D4 for LED on ESP8266 NodeMCU/D1mini
+  // Initialize LED pin
+  pinMode(ledPin, OUTPUT);
 
   // Initialize serial port
   Serial.begin(115200);
